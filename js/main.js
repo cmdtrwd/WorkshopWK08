ded91c3eb8a6ed55c9d91052ef50e8ca7a426c20
Jittrapol Int <jittrapol.i@psu.ac.th>
2023-01-05 10:21:22 +0700

    style the element with js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 // variable declaration and initialisation
-let count = 1;
+let count = 0;
 let max = 5;
 
 
 // A function to print out the number of times users click the button
 function Count(){
+    count++;
     if (count < max){
-        count++;
-        document.getElementById("title").innerHTML = count;
+        document.getElementById("title").innerHTML = count;        
+        let box = document.getElementById("box");
+        box.style.width = 100 + 50*count +"px";
+        box.style.height = 100 + 50*count +"px";
     }
     else {
         document.getElementById("title").innerHTML = "Reached the max of 5";
-        document.getElementById("box").classList.toggle("active");
+        let box = document.getElementById("box");        
+        box.classList.toggle("active");
     }    
 }
